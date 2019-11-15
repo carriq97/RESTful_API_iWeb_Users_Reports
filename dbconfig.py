4b8fbefcc6b3d21034c3f0bcd02e84d4c63bed8e
Amyrtan <martindefs@gmail.com>
2019-11-15 18:44:33 +0100

    Prueba

--- dbconfig.py
+++ dbconfig.py
@@ -4,7 +4,7 @@ mysql = MySQL()
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'martin'
 app.config['MYSQL_DATABASE_DB'] = 'iweb'
 app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
 mysql.init_app(app)
