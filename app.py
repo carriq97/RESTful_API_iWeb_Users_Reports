85c34ec92ed94e46562e76ea78dd94bc4d447c57
Manuel <manudoon_2012@hotmail.com>
2019-12-12 00:54:23 +0100

    Cambios realizados:
    -Inclusión de login OAuth(falta SQLALchemy)
    -Cambio en settings.py para añadir las claves específicas
    -Añadido archivo requirements para este repositorio

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
-from flask import Flask
-from dbconfig import mysql
+import flask_login
+from flask import Flask, g
 from resources.error_handler import errorBP
 from resources.local.report_crud import reportBP
 from resources.local.user_crud import userBP
-from resources.online.parking_openData import parkingBP
 from resources.online.location_openData import locationBP
+from resources.online.parking_openData import parkingBP
+from social_flask.routes import social_auth
 
+from dbconfig import mysql
 
 application = Flask(__name__)
+application.config.from_object('aparcamalaga.settings')
 
-# MySQL configurations
-application.config['MYSQL_USER'] = 'root'
-application.config['MYSQL_PASSWORD'] = 'password'
-application.config['MYSQL_DB'] = 'iweb'
-application.config['MYSQL_HOST'] = '127.0.0.1'
 mysql.init_app(application)
 
 # Blueprints
@@ -24,3 +22,21 @@ application.register_blueprint(userBP)
 
 application.register_blueprint(parkingBP)
 application.register_blueprint(locationBP)
+
+#OAuth
+login_manager = flask_login.LoginManager()
+
+login_manager.init_app(application)
+application.register_blueprint(social_auth)
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return int(userid)
+    except (TypeError, ValueError):
+        pass
+
+
+@application.before_request
+def global_user():
+    g.user = flask_login.current_user
